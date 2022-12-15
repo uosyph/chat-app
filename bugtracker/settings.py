28f5410412132c70cb61b3bef9636ebbb8fb2399
Yousef Saeed <107230184+yousafesaeed@users.noreply.github.com>
2022-12-15 23:43:31 +0200

    minor change

--- bugtracker/settings.py
+++ bugtracker/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "bugtracker",
+    "tracker",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
