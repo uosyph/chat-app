fc32390a50a7d823088d498ffc2a62cb6b68689e
Yousef Saeed <107230184+yousafesaeed@users.noreply.github.com>
2022-12-23 22:12:52 +0200

    SCSS to CSS script

--- tasks.sh
+++ tasks.sh
@@ -10,11 +10,14 @@ elif [[ $1 == "m" ]]; then
     python manage.py makemigrations parrhesia && python manage.py migrate
 elif [[ $1 == "r" ]]; then
     python manage.py runserver
+elif [[ $1 == "s" ]]; then
+    sass parrhesia/static/parrhesia/scss/styles.scss:parrhesia/static/parrhesia/css/styles.css
 else
     echo -e 'Usage:
 "$ tasks.sh c": Create a super user "username: super, pw: qq11qq11"
-"$ tasks.sh d": Delete the database,
+"$ tasks.sh d": Delete the database
 "$ tasks.sh m": Make & migrate the database
-"$ tasks.sh r": Run the server'
+"$ tasks.sh r": Run the server
+"$ tasks.sh s": Convert SCSS to CSS'
     exit 1
 fi
